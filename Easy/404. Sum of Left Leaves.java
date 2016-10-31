9e7a6d69676754e7eca8b269822f5758ba6a57e8
Charliebob <CharliebobCharliebob0604@gmail.com>
2016-10-31 15:28:47 -0700

    Facebook

--- Easy/404. Sum of Left Leaves.java
+++ Easy/404. Sum of Left Leaves.java
@@ -23,7 +23,12 @@ public class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
       return helper(root, false);
     }
-    private int helper(){
+    private int helper(TreeNode root, boolean isLeft){
       if(root==null) return 0;
+      if(root.left==null && root.right==null){
+        if(isLeft) return root.val;
+        return 0;
+      }
+      return helper(root.left, true) + helper(root.right, false);
     }
   }
