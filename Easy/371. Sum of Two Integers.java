e22c46629db82f4fc0353bddb4be61276541ad08
Charliebob <CharliebobCharliebob0604@gmail.com>
2016-10-31 12:25:17 -0700

    sum of two integers

---
+++ Easy/371. Sum of Two Integers.java
@@ -0,0 +1,16 @@
+// Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+//
+// Example:
+// Given a = 1 and b = 2, return 3.
+//
+// Credits:
+// Special thanks to @fujiaozhu for adding this problem and creating all test cases.
+
+public class Solution {
+    public int getSum(int a, int b) {
+      if(b==0) return a;
+      int sum = a^b;
+      int carry = (a&b)<<1;
+      return getSum(sum, carry);
+    }
+  }
