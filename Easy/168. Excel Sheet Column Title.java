e929b0cec9cbb9e39679f859031e8d853c2963b9
Charliebob <cxz131330@utdallas.edu>
2016-11-02 16:58:37 -0700

    Microsoft Facebook Zenefits

---
+++ Easy/168. Excel Sheet Column Title.java
@@ -0,0 +1,22 @@
+// Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+//
+// For example:
+//
+//     1 -> A
+//     2 -> B
+//     3 -> C
+//     ...
+//     26 -> Z
+//     27 -> AA
+//     28 -> AB
+
+public class Solution {
+    public String convertToTitle(int n) {
+      String s = "";
+      while(n!=0){
+        s = (char)('A'+(n-1)%26)+s;
+        n = (n-1)/26;
+      }
+      return s;
+    }
+  }
