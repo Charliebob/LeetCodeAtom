a50307897deb1e5945a734e6ae1852de666617af
Charliebob <cxz131330@utdallas.edu>
2016-11-02 16:27:47 -0700

    Pocket Gems Microsoft Apple Facebook

---
+++ Easy/28. Implement Strstr().java
@@ -0,0 +1,13 @@
+// Implement strStr().
+//
+// Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+      if(haystack==null || needle==null || haystack.length()<needle.length()) return -1;
+      for(int i=0; i<=haystack.length()-needle.length(); i++){
+        if(haystack.substring(i, i+needle.length()).equals(needle)) return i;
+      }
+      return -1;
+    }
+  }
