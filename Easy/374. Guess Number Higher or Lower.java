97146194ccd13be7f7a76335a894cd8e748375a0
Charliebob <cxz131330@utdallas.edu>
2016-11-02 09:53:05 -0700

    Google

---
+++ Easy/374. Guess Number Higher or Lower.java
@@ -0,0 +1,39 @@
+// We are playing the Guess Game. The game is as follows:
+//
+// I pick a number from 1 to n. You have to guess which number I picked.
+//
+// Every time you guess wrong, I'll tell you whether the number is higher or lower.
+//
+// You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+//
+// -1 : My number is lower
+//  1 : My number is higher
+//  0 : Congrats! You got it!
+// Example:
+// n = 10, I pick 6.
+//
+// Return 6.
+
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+  public int guessNumber(int n){
+    int left = 1;
+    int right = n;
+    int mid = left + (right-left)/2;
+    int guessNum = guess(mid);
+    while(guessNum!=0){
+      if(guessNum==1){
+        left = mid + 1;
+      }else if(guessNum==-1){
+        right = mid - 1;
+      }
+      mid = left + (right-left)/2;
+      guessNum = guess(mid);
+    }
+    return mid;
+  }
+}
